b6126585c54f935628cb92e8b19f26dc94f0c074
Justin Beuck <jbeuck@yahoo.com>
2016-07-20 21:44:22 -0500

    something

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: 'put-yours-here',
-    consumer_secret: 'put-yours-here',
-    access_token_key: 'put-yours-here',
-    access_token_secret: 'put-yours-here'
+    consumer_key: 'pJFYssDZ1uLYRuryleuMcPFkB',
+    consumer_secret: 'SpP5wKyTd8CqWYpqwgeguxmy7IysNKIWVBEdme7dFI5T7SLSpC',
+    access_token_key: '27757566-VjSeLfmppGQJkyuDbTX565zr6DsEfOOghT1uun8fI',
+    access_token_secret: 'NquhUviC9udyFmoIeKuexVEqUf2wNneN4hWumWYEwJUFj'
   }
 }
